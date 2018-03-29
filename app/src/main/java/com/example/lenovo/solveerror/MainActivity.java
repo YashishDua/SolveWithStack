9373ba010123058745a76186fd9241b73a861a33
YashishDua <yashish.dua@gmail.com>
2018-03-29 21:55:59 +0530

    Checking upgrades

--- app/src/main/java/com/example/lenovo/solveerror/MainActivity.java
+++ app/src/main/java/com/example/lenovo/solveerror/MainActivity.java
@@ -6,13 +6,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.text2);
-        textView.setText("check");
+        TextView textView = findViewById(R.id.text);
+        textView.setText("Check");
     }
 }
